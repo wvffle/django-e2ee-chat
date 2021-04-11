893f02620e41e8296c765d98e698292d5ce3e315
Kasper Seweryn <github@wvffle.net>
2021-04-11 21:13:54 +0200

    feat(models): add application models

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django import forms
 
-# Register your models here.
+from chat.models import Profile, Invite
+
+
+class InviteForm(forms.ModelForm):
+    invite = forms.CharField(max_length=8, min_length=8)
+
+    class Meta:
+        model = Invite
+        fields = ['invite']
+
+
+class InviteAdmin(admin.ModelAdmin):
+    form = InviteForm
+
+
+admin.site.register(Invite, InviteAdmin)
+
+# TODO: Make Profile readonly
+# TODO: Hide public key and session key
+admin.site.register(Profile)
