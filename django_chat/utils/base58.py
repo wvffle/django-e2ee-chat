c0c9f277ae9d46a29ded6ee0deaaf77941ffd428
Sylwia0506 <sysazeszkoly@gmail.com>
2021-04-12 13:10:39 +0200

    fix(base58): fix base58

--- django_chat/utils/base58.py
+++ django_chat/utils/base58.py
@@ -1,29 +1,11 @@
-import math
-import random
+import secrets
 
+alphabet = '0123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
 
-min = 7427658739644928
-max = 9007199254740992
-
-def randomInt(min, max) :
-  return math.floor(random.random() * (max - min)) + min
-
-
-
-alphabet = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
 
 def encode(num):
-    encode = ''
-    base_count = len(alphabet)
-    while (num >= base_count):
-        tmp = num / base_count
-        mod = num - base_count * tmp
-        encode = alphabet[mod] + encode
-        num = tmp
+    return ''.join([alphabet[secrets.randbelow(len(alphabet))] for i in range(num)])
 
-    if (num):
-        encode = alphabet[tmp] + encode
-    return encode
 
-def generateShortUid():
-  return encode(randomInt(min, max))
+def generate_short_uid():
+    return encode(8)
