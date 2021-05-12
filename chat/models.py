704301cf988d03dc798f5441ecc937c743b1b0d4
Sylwia0506 <63118620+Sylwia0506@users.noreply.github.com>
2021-05-12 10:54:25 +0200

    feat(backend): add login
    * fix(add-login): add login
    
    * Your commit message
    
    * Commit for the Submit form
    
    * Fix caching
    
    Co-authored-by: Kasper Seweryn <github@wvffle.net>

--- chat/models.py
+++ chat/models.py
@@ -5,12 +5,18 @@ from django.db import models
 class Invite(models.Model):
     invite = models.CharField(max_length=8)
 
+    def __str__(self):
+        return self.invite
+
 
 class Profile(models.Model):
     invite = models.ForeignKey(Invite, on_delete=models.CASCADE)
     name = models.CharField(max_length=8)
     session_key = models.CharField(max_length=32)
     public_key = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return self.name
 
 
 class Room(models.Model):
@@ -27,3 +33,12 @@ class Message(models.Model):
 class Ban(models.Model):
     invite = models.OneToOneField(Invite, on_delete=models.CASCADE, null=True, blank=True)
     profile = models.OneToOneField(Profile, on_delete=models.CASCADE, null=True, blank=True)
+
+    def is_banned(self, profile):
+        return profile == self.profile or profile.invite == self.invite
+
+    def __str__(self):
+        if self.invite:
+            return 'invite-' + str(self.invite)
+
+        return 'profile-' + str(self.profile)
