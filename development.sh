ef2dfdf7a8174f2c227d962bcaf9eda430877fe8
Kasper Seweryn <github@wvffle.net>
2021-06-14 16:27:28 +0200

    fix(docker): add pillow dependencies

--- development.sh
+++ development.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-apk add gcc g++ make libffi-dev openssl-dev python3-dev build-base
+apk add gcc g++ make libffi-dev openssl-dev python3-dev build-base linux-headers zlib-dev jpeg-dev musl-dev cargo
 
 yarn install
 poetry install
